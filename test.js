1b9313b41cd6fb8d69efa4d87d4d5d47c249dda6
leslie-liz <lerflizz1314@hotmail.com>
2024-05-06 14:45:30 +0800

    sync the main branch

--- test.js
+++ test.js
@@ -3,3 +3,20 @@ function test() {
   console.log(y);
 }
 test();
+function test (a, b) {
+    let num1 = a;
+    let num2 = b
+    return a + b;
+}
+
+test (5, 15)
+function test() {
+    console.log('Test')
+    console.log('Hi Haziq')
+}
+
+
+const x = "name"
+
+test();
+const y = "This is to test"
