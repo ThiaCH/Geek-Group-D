33fbb7a2ad10082a49a64e0d41dacd2f6bb97ec3
leslie-liz <lerflizz1314@hotmail.com>
2024-05-08 13:48:23 +0800

    Clean up miscelleneous

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, "dist", "vite.svg")));
 app.use(express.static(path.join(__dirname, "dist")));
 
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
 const port = process.env.PORT || 3000;
