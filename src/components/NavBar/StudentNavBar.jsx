52d0d0e359f6e0576816258470b549903a05dd16
leslie-liz <lerflizz1314@hotmail.com>
2024-05-16 16:23:10 +0800

    regular commit

---
+++ src/components/NavBar/StudentNavBar.jsx
@@ -0,0 +1,26 @@
+import {NavLink} from 'react-router-dom'
+import * as userService from '../../utilities/users-service'
+import '../../css/styles.css'
+
+export default function NavBar({setUser}) {
+
+    function handleLogOut() {
+      // Delegate to the users-service
+      userService.logOut();
+      // Update state will also cause a re-render
+      setUser(null);
+    }
+  
+      return (
+        <nav className="navbar">
+          <div className="navbar-container">
+            <div className="spacer"></div>
+              <div className="options-container">
+                <NavLink to="/dashboard">Dashboard</NavLink>
+                <span className="separator"> | </span>
+                <NavLink to="/" onClick={handleLogOut} className="logout-link">Log Out</NavLink>
+            </div>
+          </div>
+        </nav>
+      )
+  }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,21 +1,21 @@
 import debug from "debug"
 import { useState, useEffect } from "react";
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom' // eslint-disable-line no-unused-vars
 import NavBar from "../../components/NavBar/NavBar";
 import AdminPage from "../AdminPage/AdminPage";
 import { getUser } from "../../utilities/users-service";
 import AuthPage from "../AuthPage/AuthPage";
+import StudentNavBar from "../../components/NavBar/StudentNavBar"
 // import StudentSignUp from "../StudentSignUpPage/StudentSignUpPage";
 
 
 // this enables debug module at the App.jsx only, this replaces console.log, you can see it at the browser devtool, enable the verbose level at web console
-const log = debug('mern:pages:App:App');
+const log = debug('mern:pages:App:App'); // eslint-disable-line no-unused-vars
 localStorage.debug = 'mern:*';
 
 export default function App() {
   // eslint-disable-next-line no-unused-vars
   const [user, setUser] = useState(getUser()); // getUser()
-  log("Test this is inside the App");
 
   const[newTime, setNewTime] = useState(new Date().getDate()); // eslint-disable-line no-unused-vars
   
@@ -35,19 +35,24 @@ export default function App() {
         <AuthPage setUser={setUser} />
       </main>
     );
-  }
-
-  if (user && user.role === 'admin') {
-    return <Navigate to="/admin" />;
-  }
-
-  return (
-    <>
+  } else if (user && user.isAdmin === true) {
+    return (
+      <>
       <main className="App">
         <NavBar setUser={setUser}/>
         <Routes>
           <Route path='/admin' element={<AdminPage />} />
         </Routes>
+      </main>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <main className="App">
+        <StudentNavBar setUser={setUser}/>
+        <p>student dashboard page here</p>
       </main>
     </>
   );
