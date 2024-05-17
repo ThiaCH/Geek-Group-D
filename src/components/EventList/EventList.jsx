3fdbaae64b862ea8028c40a077deeaa3c586db66
CHThia <capitoljazzakat@gmail.com>
2024-05-17 22:17:34 +0800

    Update: Setup basic styling layout for Dashboard

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,5 +1,5 @@
 
-export default function EventList() {
+export default function EventList () {
 
   return (
     <>
--- src/pages/DashBoardPage/DashBoardPage.jsx
+++ src/pages/DashBoardPage/DashBoardPage.jsx
@@ -1,9 +1,31 @@
-
+import '../../css/dashboard.css'
 
 export default function DashBoard () {
   return (
     <>
-      <h1>DashBoard Page</h1>
+      <div className='main-dashboard-container'> 
+        <div className='unit-overview'>
+          <h1>Unit Lesson Overview</h1>
+          <ul>
+            <li>To map list from Data</li>
+          </ul>
+        </div>
+        
+        <div className='other-container'>
+          <div className='upcoming-events'>
+            <h1>Upcoming Events</h1>
+            <ul>
+            <li>To map list from Data</li>
+            </ul>
+          </div>
+          <div className='resources'>
+            <h1>Resources</h1>
+            <ul>
+            <li>To map list from Data</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
