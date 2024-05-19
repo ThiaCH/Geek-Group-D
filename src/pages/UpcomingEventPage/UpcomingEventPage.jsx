097e2087803f826af68cdee14bb25ce1d295ee0d
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-05-19 20:20:07 +0800

    feat: fetchEvents and add useState for events

--- src/pages/UpcomingEventPage/UpcomingEventPage.jsx
+++ src/pages/UpcomingEventPage/UpcomingEventPage.jsx
@@ -1,14 +1,35 @@
-import EventList from '../../components/EventList/EventList'
-import AddEventForm from '../../components/AddEventForm/AddEventForm'
-import '../../css/event.css'
+import EventList from "../../components/EventList/EventList";
+import AddEventForm from "../../components/AddEventForm/AddEventForm";
+import "../../css/event.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default function UpcomingEvent () {
+export default function UpcomingEvent() {
+  const [events, setEvents] = useState([]);
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("/api/events");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch events");
+      }
+      const eventData = await response.json();
+      setEvents(eventData);
+    } catch (error) {
+      console.error("Error fetching events:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
     <>
       <div className="main-event-container">
-        <EventList />
+        <EventList events={events} />
         <AddEventForm />
       </div>
     </>
-  )
+  );
 }
