a91000c4cd3bc2cd79ef1139e8792eef88f70f7b
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-05-20 14:03:11 +0800

    feat: fetch user class and input inside url to display event based on class

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,68 +2,67 @@ import { useState } from "react";
 import * as usersService from "../../utilities/users-service";
 import { useNavigate, Link } from "react-router-dom";
 
-export default function LoginForm({setUser}) {
-    const navigate = useNavigate();
-    
-    const [credentials, setCredentials] = useState({
-        email: "",
-        password: "",
-    });
+export default function LoginForm({ setUser }) {
+  const navigate = useNavigate();
 
-    const [error, setError] = useState("");
-  
-    function handleChange(evt) {
-      setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
-      setError("");
-    }
-    
-    // cronjob function -> string -> usersService.createUrl(string); -> url -> database
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
-    async function handleSubmit(evt) {
-      // Prevent form from being submitted to the server
-      evt.preventDefault();
-      try {
-        // The promise returned by the signUp service method
-        // will resolve to the user object included in the
-        // payload of the JSON Web Token (JWT)
-        const user = await usersService.login(credentials);
-        setUser(user);
-        if (user.isAdmin === true) {
-          navigate("/admin");
-        } else {
-          navigate("/dashboard");
-        }
-      } catch {
-        setError("Log In Failed - Try Again");
+  const [error, setError] = useState("");
+
+  function handleChange(evt) {
+    setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
+    setError("");
+  }
+
+  // cronjob function -> string -> usersService.createUrl(string); -> url -> database
+
+  async function handleSubmit(evt) {
+    // Prevent form from being submitted to the server
+    evt.preventDefault();
+    try {
+      // The promise returned by the signUp service method
+      // will resolve to the user object included in the
+      // payload of the JSON Web Token (JWT)
+      const user = await usersService.login(credentials);
+      setUser(user);
+      if (user.isAdmin === true) {
+        navigate("/admin");
+      } else {
+        navigate(`/${user.class}/dashboard`);
       }
+    } catch {
+      setError("Log In Failed - Try Again");
     }
-  
-    return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={handleSubmit}>
-            <label>Email</label>
-            <input
-              type="text"
-              name="email"
-              value={credentials.email}
-              onChange={handleChange}
-              required
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={credentials.password}
-              onChange={handleChange}
-              required
-            />
-            <button type="submit">LOG IN</button>
-          </form>
-        </div>
-        <Link to='/signup'>Does not have an account? Sign Up here!</Link>
-        <p className="error-message">&nbsp;{error}</p>
+  }
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={credentials.email}
+            onChange={handleChange}
+            required
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
       </div>
-    )
+      <Link to="/signup">Does not have an account? Sign Up here!</Link>
+      <p className="error-message">&nbsp;{error}</p>
+    </div>
+  );
 }
-        
