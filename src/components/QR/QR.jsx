907538e7161a9d6e7912b2c8d1a2d0aed7ad4edf
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-05-22 18:21:44 +0800

    feat: add last generated time for qr

--- src/components/QR/QR.jsx
+++ src/components/QR/QR.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 export default function QR({ handleGenerateIds, validLoginIds }) {
+  const [lastClickTime, setLastClickTime] = useState(null);
+  const handleButtonClick = () => {
+    handleGenerateIds();
+    setLastClickTime(new Date());
+  };
+
   return (
     <>
-      <button onClick={handleGenerateIds}>Generate IDs</button>
+      <button onClick={handleButtonClick}>Generate IDs</button>
+      {lastClickTime && (
+        <p>
+          Last Generated: {lastClickTime.toLocaleString('en-SG')}
+        </p>
+      )}
       <img
-        src={`http://api.qrserver.com/v1/create-qr-code/?data=https://project-3-geekery.onrender.com/${validLoginIds}&size=100x100`}
+        src={`http://api.qrserver.com/v1/create-qr-code/?data=https://project-3-geekery.onrender.com/${validLoginIds}&size=300x300`}
       />
     </>
   );
