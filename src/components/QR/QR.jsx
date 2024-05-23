68b3052414298d108b56c58ba9566943099a9af6
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-05-23 09:09:15 +0800

    feat: display QR on click

--- src/components/QR/QR.jsx
+++ src/components/QR/QR.jsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 
 export default function QR({ handleGenerateIds, validLoginIds }) {
   const [lastClickTime, setLastClickTime] = useState(null);
+  const [idsGenerated, setIdsGenerated] = useState(false);
   const handleButtonClick = () => {
     handleGenerateIds();
     setLastClickTime(new Date());
+    setIdsGenerated(true);
   };
 
   return (
     <>
       <button onClick={handleButtonClick}>Generate IDs</button>
       {lastClickTime && (
-        <p>
-          Last Generated: {lastClickTime.toLocaleString('en-SG')}
-        </p>
+        <p>Last Generated: {lastClickTime.toLocaleString("en-SG")}</p>
       )}
-      <img
-        src={`http://api.qrserver.com/v1/create-qr-code/?data=https://project-3-geekery.onrender.com/${validLoginIds}&size=300x300`}
-      />
+      {idsGenerated && (
+        <img
+          src={`http://api.qrserver.com/v1/create-qr-code/?data=https://project-3-geekery.onrender.com/${validLoginIds}&size=300x300`}
+        />
+      )}
     </>
   );
 }
